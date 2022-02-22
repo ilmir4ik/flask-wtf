4d24dc20a3aa7f6aaa44d51bef9e432d976f2eac
Almet-Agliev4 <k.agliev@mail.ru>
2022-02-22 09:42:01 +0300

    Тренировки в полёте

--- main.py
+++ main.py
@@ -9,5 +9,19 @@ def index(title):
     return render_template('base.html', title=title)
 
 
+@app.route('/training/<prof>')
+def train(prof):
+    if 'врач' in prof.lower() or 'учен' in prof.lower() or 'учён' in prof.lower() or 'ном' in prof.lower():
+        res = 'Научные симуляторы'
+    elif 'инжен' in prof.lower() or 'строит' in prof.lower():
+        res = 'Инженерные тренажеры'
+    else:
+        res = 'Другое'
+    title = host + ':' + str(port) + '/training/' + prof
+    return render_template('index.html', title=title, text=res)
+
+
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    port = 8080
+    host = '127.0.0.1'
+    app.run(port=port, host=host)
